bdde64eee850051be69f42dde041b511d2ac84af
Kallinikos <84861402+Kall-K@users.noreply.github.com>
2024-06-23 18:43:46 +0300

    one more commit

--- PartB/ga.py
+++ PartB/ga.py
@@ -1,104 +1,273 @@
-# One Max Problem
-# https://deap.readthedocs.io/en/master/examples/ga_onemax.html
-
 import random
+from numpy.random import randint
+from numpy.random import rand
+import json
+import pandas as pd
+import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import matplotlib.pyplot as plt
+import csv
+import os
+import shutil
 
-from deap import base
-from deap import creator
-from deap import tools
+# create folder and if exists clear folder contents
+figures_path = 'PartB/figures/'
+results_path = 'PartB/results/'
 
-#Create a FitnessMax class 
-creator.create("FitnessMax", base.Fitness, weights=(1.0,))
-#Create an Individual class 
-creator.create("Individual", list, fitness=creator.FitnessMax)
+def clear_folder(path):
+    if os.path.exists(path):
+        shutil.rmtree(path)
+    os.mkdir(path)
 
-#The toolbox is a container for functions with their arguments
-toolbox = base.Toolbox()
-# Attribute generator 
-toolbox.register("attr_bool", random.randint, 0, 1)
-# Structure initializers
-toolbox.register("individual", tools.initRepeat, creator.Individual, toolbox.attr_bool, 100)
-toolbox.register("population", tools.initRepeat, list, toolbox.individual) 
+clear_folder(figures_path)
+clear_folder(results_path)
 
+# load texts and top k texts
+df = pd.read_csv('PartB/region1693.csv', encoding='utf-8')
+TEXTS = df['text'].values.tolist()
 
-def evalOneMax(individual):
-    return sum(individual),
-	
+with open('PartB/topk_texts.json', 'r', encoding='utf-8') as f:
+    TOP_K = json.load(f)
 
-# Register the genetic operators	
-toolbox.register("evaluate", evalOneMax)
-toolbox.register("mate", tools.cxTwoPoint)
-toolbox.register("mutate", tools.mutFlipBit, indpb=0.05)
-toolbox.register("select", tools.selTournament, tournsize=3)
+# auxiliary functions
+def load_token(dec_num):
+	with open('PartB/token_id.json', 'r', encoding='utf-8') as f:
+		tokens = json.load(f)
+	return tokens.get(str(dec_num))
 
+def bin2dec(binary):
+    dec = 0
+    for b in range(len(binary)):
+        dec += binary[-b-1]*pow(2,b)
+    return dec
+
+def dec2bin(decimal):
+    bin_num = bin(decimal)[2:]
+    bin_num = bin_num.zfill(11)
+    return [int(b) for b in bin_num]
+
+def check_range(num):
+    num = bin2dec(num)
+    if num not in range(0,1677):
+        num = random_remapping()
+    return dec2bin(num)
+
+def random_remapping():
+    num = randint(0,1677)
+    if num in range(0,1677):
+        return num
+    else:
+        random_remapping()
+
+# genetic algorith class
+class GeneticAlgorithm:
+    def __init__(self, n_bits, n_iter, n_pop, r_cross, r_mut):
+        self.n_pop = n_pop
+        self.r_cross = r_cross
+        self.r_mut = r_mut
+        self.n_bits = n_bits
+        self.n_iter = n_iter
+        self.objective = self.cos_similarity
+        self.stagnation_threshold = 50  # Number of generations with no significant improvement
+        self.improvement_threshold = 0.01  # Improvement percentage threshold
+
+    # objective function
+    def cos_similarity(self, x):
+        main_text = load_token(bin2dec(x[:11])) + ' αλεξανδρε ουδις ' + load_token(bin2dec(x[11:]))
+        all_texts = TEXTS + [main_text] 
+        tfidf_matrix = TfidfVectorizer().fit_transform(all_texts)
+        
+        cosine_sim_matrix = cosine_similarity(tfidf_matrix)
+        indexes = [i['id'] for i in TOP_K]
+        cosine_sim = []
+        for i in range(len(cosine_sim_matrix[-1])):
+            if i in indexes:
+                cosine_sim.append(cosine_sim_matrix[-1][i])
+            
+        return sum(cosine_sim)/len(cosine_sim)
+
+    # selection
+    def roulette_wheel_selection(self, population, fitness_scores):
+        total_fitness = sum(fitness_scores)
+        relative_fitness = [f / total_fitness for f in fitness_scores]
+        cumulative_probability = [sum(relative_fitness[:i+1]) for i in range(len(relative_fitness))]
+
+        rand = random.random()
+        for i, cp in enumerate(cumulative_probability):
+            if rand <= cp:
+                return population[i]
+
+    # crossover two parents to create two children
+    def crossover(self, p1, p2, r_cross):
+        # children are copies of parents by default
+        c1, c2 = p1.copy(), p2.copy()
+        # check for recombination
+        if rand() < r_cross:
+            # select crossover point that is not on the end of the string
+            pt = randint(1, len(p1)-2)
+            # perform crossover
+            c1 = p1[:pt] + p2[pt:]
+            c2 = p2[:pt] + p1[pt:]
+
+            pt2 = randint(1, len(p1)-2)
+            while ():
+                if pt2 != pt:
+                    break
+                else :
+                    pt2 = randint(1, len(p1)-2)
+            c1 = c1[:pt2] + c2[pt2:]
+            c2 = c2[:pt2] + c1[pt2:]
+
+        return [c1, c2]
+
+    # mutation operator
+    def mutation(self, bitstring, r_mut):
+        for i in range(len(bitstring)):
+            # check for a mutation
+            if rand() < r_mut:
+                # flip the bit
+                bitstring[i] = 1 - bitstring[i]
+
+    # genetic algorithm
+    def genetic_algorithm(self):
+        # initial population of random bitstring
+        pop = []
+        for _ in range(self.n_pop):
+            new = randint(0, 2, self.n_bits).tolist()
+            pop.append(check_range(new[:11])+check_range(new[11:]))
+
+        # keep track of best solution
+        best, best_eval = pop[0], self.objective(pop[0])
+
+        # initialization of variables
+        elits = []
+        no_improvement_generations = 0
+
+        # enumerate generations
+        for gen in range(self.n_iter):
+            # evaluate all candidates in the population
+            scores = [self.objective(c) for c in pop]
+            # find the elite of this gene
+            elit_val = max(scores)
+            elit_index = [i for i, s in enumerate(scores) if s == elit_val]
+            # store the elit of this gene
+            elits.append(elit_val)
+
+            # Termination criteria
+            if len(elits) > 1:
+                if elit_val <= elits[-2]:
+                    no_improvement_generations += 1
+                    criteria1 = no_improvement_generations >= self.stagnation_threshold
+                    if criteria1:
+                        print(f"Terminated at generation {gen+1}: criterion 1 (stagnation={self.stagnation_threshold}) ")
+                        break
+                else:
+                    no_improvement_generations = 0
+                    criteria2 = ((elit_val - elits[-2])/elits[-2]) < self.improvement_threshold
+                    if criteria2:
+                        print(f"Terminated at generation {gen+1}: criterion 2 (improvement={self.improvement_threshold})")
+                        break
+                if gen == self.n_iter - 1:
+                    print("Terminated due to reaching the maximum number of generations")
+
+            # check for new best solution
+            for i in range(self.n_pop):
+                len(scores)
+                if scores[i] > best_eval:
+                    best, best_eval = pop[i], scores[i]
+                    print(">%d, new best f(%s) = %.3f" % (gen+1,  pop[i], scores[i]))
+            # select parents
+            selected = [self.roulette_wheel_selection(pop, scores) for _ in range(self.n_pop)]
+            # create the next generation
+            children = list()		
+            for i in range(0, self.n_pop, 2):
+                # get selected parents in pairs
+                p1, p2 = selected[i], selected[i+1]
+
+                mutation = True
+                elit = 0
+                if i in elit_index:
+                    mutation = False
+                    elit = 1
+                if i+1 in elit_index:
+                    mutation = False
+                    elit = 2
+                
+                iter = 0
+                for c in self.crossover(p1, p2, self.r_cross):
+                    iter += 1
+                    # mutation
+                    if (mutation == False and iter == elit):
+                        # store for next generation without mutation
+                        children.append(check_range(c[:11])+check_range(c[11:]))
+                    else:
+                        self.mutation(c, self.r_mut)
+                        # store for next generation
+                        children.append(check_range(c[:11])+check_range(c[11:]))
+                   
+            # replace population
+            pop = children
+        return [best, best_eval, elits]
+
+def run_ga(n_bits, n_iter, n_pop, r_cross, r_mut):
+            ga = GeneticAlgorithm(n_bits, n_iter, n_pop, r_cross, r_mut)
+            best, best_eval, elits = ga.genetic_algorithm()
+            return best, best_eval, elits
 
 def main():
+    with open('PartB/experiments.json', 'r', encoding='utf-8') as f:
+        exp = json.load(f)
+    for num, e in enumerate(exp):
+        # define the total iterations
+        n_iter = 1000
+        # bits
+        n_bits = 22
+        # define the population size
+        n_pop = e['n_pop'] #[20, 200]
+        # crossover rate
+        r_cross = e['r_cross'] #[0.1, 0.6, 0.9]
+        # mutation rate
+        r_mut = e['r_mut'] #[0, 0.01, 0.1]            
+        # perform the genetic algorithm search
+        print(f'>Experiment {num+1}: n_pop={n_pop}, r_cross={r_cross}, rmut={r_mut}')
+        print('>>>Start')
 
-  # Create population
-  pop = toolbox.population(n=300)
-	
-  # Evaluate the entire population
-  fitnesses = list(map(toolbox.evaluate, pop))
-  for ind, fit in zip(pop, fitnesses):
-      ind.fitness.values = fit
-  
-  # CXPB  is the probability with which two individuals are crossed
-  # MUTPB is the probability for mutating an individual
-  CXPB, MUTPB = 0.5, 0.2
+        fields = ['population size', 'crossover probability', 'mutation probability', 'best', 'generations', 'text']
+        all_elits = np.zeros((10, 1000))
+        avg_best_eval = 0
+        avg_gen = 0
+        with open(f'PartB/results/experiment{num+1}.csv', 'w', encoding='utf-8') as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=fields, lineterminator='\n')
+            writer.writeheader()
+            for i in range(10):
+                best, best_eval, elits = run_ga(n_bits, n_iter, n_pop, r_cross, r_mut)
+                avg_best_eval += best_eval
+                avg_gen += len(elits)
+                text = load_token(bin2dec(best[:11])) + ' αλεξανδρε ουδις ' + load_token(bin2dec(best[11:]))
+                print('>Done!')
+                print('f(%s) = %f' % (best, best_eval))
+                print(f'word 1:{bin2dec(best[:11])}\tword 2:{bin2dec(best[11:])}')
+                print(text)
+                print('-----------------------------------------------------------------------------')
+                for j in range(len(elits)):
+                    all_elits[i][j] = elits[j]
+                writer.writerow({'population size':n_pop,'crossover probability':r_cross,
+                                  'mutation probability':r_mut, 'best':best_eval, 'generations':len(elits), 'text':text})
+            writer.writerow({'population size':n_pop,'crossover probability':r_cross,
+                              'mutation probability':r_mut, 'best':avg_best_eval/10, 'generations':avg_gen/10})
+        column_averages = np.mean(all_elits, axis=0)
+        x_values = np.arange(1, 1001)
 
-	# Extracting all the fitnesses of 
-  fits = [ind.fitness.values[0] for ind in pop]
-	
-  # Variable keeping track of the number of generations
-  g = 0
-  
-  # Begin the evolution
-  while max(fits) < 100 and g < 1000:
+        # Plotting the data
+        plt.figure(figsize=(10, 5))
+        plt.plot(x_values, column_averages, label='Generation Averages')
+        plt.xlabel('Generation')
+        plt.ylabel('Average Elite Value')
+        plt.title(f'Average Elite Value of each Generation, Exp:{num+1}')
+        plt.legend()
+        # plt.show()
+        plt.savefig(f'PartB/figures/experiment{num+1}.png')
+        print('>>>End')
 
-    # A new generation
-    g = g + 1
-    print("-- Generation %i --" % g) 
-
-    # Select the next generation individuals
-    offspring = toolbox.select(pop, len(pop))
-    # Clone the selected individuals
-    offspring = list(map(toolbox.clone, offspring)) 
-
-    # Apply crossover and mutation on the offspring
-    for child1, child2 in zip(offspring[::2], offspring[1::2]):
-        if random.random() < CXPB:
-            toolbox.mate(child1, child2)
-            del child1.fitness.values
-            del child2.fitness.values
-
-    for mutant in offspring:
-        if random.random() < MUTPB:
-            toolbox.mutate(mutant)
-            del mutant.fitness.values
-
-
-    # Evaluate the individuals with an invalid fitness
-    invalid_ind = [ind for ind in offspring if not ind.fitness.valid]
-    fitnesses = map(toolbox.evaluate, invalid_ind)
-    for ind, fit in zip(invalid_ind, fitnesses):
-        ind.fitness.values = fit
-
-    # Replace the old population by the offspring
-    pop[:] = offspring  
-
-    # Gather all the fitnesses in one list and print the stats
-    fits = [ind.fitness.values[0] for ind in pop]
-
-    length = len(pop)
-    mean = sum(fits) / length
-    sum2 = sum(x*x for x in fits)
-    std = abs(sum2 / length - mean**2)**0.5
-
-    print("  Min %s" % min(fits))
-    print("  Max %s" % max(fits))
-    print("  Avg %s" % mean)
-    print("  Std %s" % std)
-		
-	
-# Run GA Algorithm
 main()
--- PartB/prepare_data.py
+++ PartB/prepare_data.py
@@ -3,14 +3,9 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 import json
 import os
 from sklearn.metrics.pairwise import cosine_similarity
-from numpy.random import randint#######
-import csv
+from numpy.random import randint
 
-NGRAM = (1,1) 
-TOKEN_MODE = 'word'
-MIN_DOCUMENT_FREQUENCY = 1 # all the words
 
-token_idf = {}
 token_num = {}
 
 def bin2dec(binary):
@@ -37,38 +32,6 @@ def random_remapping():
     else:
         random_remapping()
 
-def test():
-    n_pop = 5
-    n_bits = 22
-    pop = []
-    for _ in range(n_pop):
-        new = randint(0, 2, n_bits).tolist()
-        pop.append(check_range(new[:11])+check_range(new[11:]))
-
-    print(pop)
-    for p in pop:
-        print(bin2dec(p[:11]),bin2dec(p[11:]))
-    similarity(pop[0])
-    # bin = randint(0, 2, 22).tolist() 
-    # first_bin = bin[:11]
-    # second_bin = bin[11:]
-    # print(bin)
-
-    # first_num = bin2dec(first_bin)
-    # print(first_num)
-
-    # second_num = bin2dec(second_bin)
-    # print(second_num)
-    # check_range(second_num)
-
-    # print("after range check:")
-    # first_num = check_range(first_num)
-    # second_num = check_range(second_num)
-    # print(first_num)
-    # print(second_num)
-    # print('decimal to bin:')
-    # print(dec2bin(first_num))
-    # print(dec2bin(second_num))
 
 def import_data():
     file_path = "PartB/iphi2802.csv"
@@ -76,142 +39,42 @@ def import_data():
     df = pd.read_csv(file_path, encoding='utf-8', delimiter='\t')
 
     filtered_df = df[df['region_main_id'] == 1693]
+    filtered_df.to_csv('PartB/region1693.csv', index=False, encoding='utf-8')
     texts = filtered_df['text'].values.tolist()
     texts.append('[...] αλεξανδρε ουδις [...]')
 
     filtered_out = ngram_vectorize(texts)
 
-    print(cos_similarity(filtered_out))
     with open('PartB/topk_texts.json', 'w', encoding='utf-8') as fp:
         json.dump(cos_similarity(filtered_out), fp, ensure_ascii=False)
 
-    pd.DataFrame(filtered_out.toarray()).to_csv("PartB/tfidf_matrix.csv", index=False)
-
-    print(len(token_idf),len(token_num))
-
-    os.remove('PartB/token_tfidf.json')
-    with open('PartB/token_tfidf.json', 'w', encoding='utf-8') as fp:
-        json.dump(token_idf, fp, ensure_ascii=False)
-    
-    os.remove('PartB/token_id.json')
+    if os.path.exists('PartB/token_id.json'):
+        os.remove('PartB/token_id.json')
     with open('PartB/token_id.json', 'w', encoding='utf-8') as fp:
         json.dump(token_num, fp, ensure_ascii=False)
-
+    
 
 def ngram_vectorize(x):
-    # Create keyword arguments to pass to the 'tf-idf' vectorizer.
-    kwargs = {
-            'ngram_range': NGRAM,  # Use 1-grams.
-            'strip_accents': 'unicode',
-            'decode_error': 'ignore',
-            'analyzer': TOKEN_MODE,  # Split text into word tokens.
-            'min_df': MIN_DOCUMENT_FREQUENCY
-    }
-
-    vectorizer = TfidfVectorizer(**kwargs)
+    vectorizer = TfidfVectorizer()
     X = vectorizer.fit_transform(x)
-
-    counter=0
-
+    # here all tokens are stored with their ID
+    # so I know which number from 0 to 1677 corresponds to each word
     feature_names = vectorizer.get_feature_names_out()
+    counter=0
     for i, x in enumerate(x):
-        # print(f"Document {i+1}:")
         feature_index = X[i,:].nonzero()[1]
         tfidf_scores = zip(feature_index, [X[i, x] for x in feature_index])
         for feature_index, score in tfidf_scores:
-            # print(f"{feature_names[feature_index]}: {score}")
             if feature_names[feature_index] not in token_num.values():
                 token_num.update({counter:feature_names[feature_index]})
                 counter +=1
-            token_idf.update({feature_names[feature_index]: score})
     return X
 
 
 def cos_similarity(tfidf_matrix):
-    specific_text_vector = tfidf_matrix[-1]
-    cosine_similarities = cosine_similarity(tfidf_matrix[:-1], specific_text_vector)
-    cosine_similarities = cosine_similarities.flatten()
+    cosine_sim_matrix = cosine_similarity(tfidf_matrix)
+    similarities = cosine_sim_matrix[-1][:-1]
+    return [{'id':i,'similarity':cos} for i,cos in enumerate(similarities) if cos!=0]
 
-    return [{'id':i+1,'similarity':cos} for i,cos in enumerate(cosine_similarities) if cos!=0]
+import_data()
 
-def load_top_k():
-	with open('PartB/topk_texts.json', 'r', encoding='utf-8') as f:
-		top_k = json.load(f)
-
-	rows = [0]
-	for rec in top_k:
-		rows.append(rec['id'])
-
-	# df = pd.read_csv('PartB/region1693.csv', skiprows = lambda x: x not in rows)
-	df = pd.read_csv('PartB/region1693.csv', skiprows = lambda x: x not in rows)
-	return df['text'].values.tolist()
-	# texts.append('[...] αλεξανδρε ουδις [...]')
-	
-	# return df_vect
-
-def load_token(dec_num):
-    with open('PartB/token_id.json', 'r', encoding='utf-8') as f:
-        tokens = json.load(f)
-    # print(tokens[str(dec_num)])
-    # if dec_num in tokens:
-    return tokens.get(str(dec_num))
-    # else:
-    #     return None
-
-def ngram_vectorizer(x):
-	# Create keyword arguments to pass to the 'tf-idf' vectorizer.
-	kwargs = {
-			'ngram_range': NGRAM,  # Use 1-grams.
-			'strip_accents': 'unicode',
-			'decode_error': 'ignore',
-			'analyzer': TOKEN_MODE,  # Split text into word tokens.
-			'min_df': MIN_DOCUMENT_FREQUENCY
-	}
-
-	vectorizer = TfidfVectorizer(**kwargs)
-	X = vectorizer.fit_transform(x)
-	return X
-
-def similarity(x):
-	# score = 0
-    texts = load_top_k()
-    first = load_token(bin2dec(x[:11]))
-    second = load_token(bin2dec(x[11:]))
-    text = first + ' αλεξανδρε ουδις ' + second
-    
-    texts.append(text)
-    print(texts)
-    tfidf_matrix = ngram_vectorizer(texts)
-    # print(tfidf_matrix)
-    specific_text_vector = tfidf_matrix[-1]
-    cosine_similarities = cosine_similarity(tfidf_matrix[:-1], specific_text_vector)
-    cosine_similarities = cosine_similarities.flatten()
-    print(cosine_similarities)
-    print(sum(cosine_similarities)/len(cosine_similarities))
-    return sum(cosine_similarities)/len(cosine_similarities)
-	# return [{'id':i+1,'similarity':cos} for i,cos in enumerate(cosine_similarities) if cos!=0]
-	# return [{'id':i+1,'similarity':cos} for i,cos in enumerate(cosine_similarities)]
-
-
-
-
-
-# test()
-# import_data()
-
-def termination_check(elits, gens):
-    rate = ((elits[-1]-elits[-2])/elits[-1])*100
-    print(rate)
-    c = 0
-    for i in range(gens):
-        if elits[-i-1] - elits[-i-2] < 0:
-            print("smaller")
-            c += 1
-        else: 
-            break
-    if rate < 1 or c == gens:
-        return True
-    else:
-        return False
-     
-print(termination_check([1,2,3,4,10,6,5,7.01], 3))
